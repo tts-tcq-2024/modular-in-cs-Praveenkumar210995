254c12ef503a324c0bb94b43ab67d31ef59086bf
Praveenkumar210995 <pravin21995@gmail.com>
2024-09-26 18:47:43 +0530

    Update ColorMapping.cs

--- ColorMapping.cs
+++ ColorMapping.cs
@@ -5,6 +5,6 @@ namespace TelCo.ColorCoder
     public class ColorMapping
     {
         public readonly Color[] MajorColorMapping = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet};
-        public readonly Color[] MinorColorMapping = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGrey};
+        public readonly Color[] MinorColorMapping = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray};
     }
 }
